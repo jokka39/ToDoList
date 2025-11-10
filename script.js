386148bf12fdbaa9101649c225dbb5ae68d646c5
JK <grayart1492@gmail.com>
2025-11-10 03:33:07 +0300

    Enhance addTask function with delete button
    Refactor addTask function to include delete button functionality and clean up input after adding a task.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 const addTaskButton = document.getElementById('addTaskButton');
 
-// Функция для добавления задачи
 function addTask() {
     const taskText = taskInput.value;
     if (taskText === '') return;
@@ -10,12 +9,11 @@ function addTask() {
     const li = document.createElement('li');
     li.innerText = taskText;
 
-    // Обработчик для отметки задачи как выполненной
     li.onclick = () => {
         li.classList.toggle('completed');
     };
 
-    // Кнопка удаления задачи
+
     const deleteButton = document.createElement('button');
     deleteButton.innerText = 'Удалить';
     deleteButton.onclick = (event) => {
@@ -25,13 +23,12 @@ function addTask() {
 
     li.appendChild(deleteButton);
     taskList.appendChild(li);
-    taskInput.value = ''; // Очищаем input
+    taskInput.value = ''; 
 }
 
-// Слушатель события для кнопки добавления
+
 addTaskButton.addEventListener('click', addTask);
 
-// Слушатель события для нажатия Enter в поле ввода
 taskInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         addTask();
